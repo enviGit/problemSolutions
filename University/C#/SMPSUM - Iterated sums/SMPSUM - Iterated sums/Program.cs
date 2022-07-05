6d6a2d325b550e44df3cbb960d99492ee7fff23b
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2022-07-06 00:20:19 +0200

    SMPSUM - Iterated sums solution added

---
+++ University/C#/SMPSUM - Iterated sums/SMPSUM - Iterated sums/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SMPSUM___Iterated_sums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linia = Console.ReadLine();
+            string[] tab = linia.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int a = int.Parse(tab[0]);
+            int b = int.Parse(tab[1]);
+            int suma = 0;
+
+            for (int i = a; i <= b; i++)
+                suma = suma + i * i;
+
+            Console.Write(suma);
+        }
+    }
+}
