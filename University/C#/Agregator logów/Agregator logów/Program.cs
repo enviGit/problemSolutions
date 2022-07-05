81541f2e270009bdae5b80b644293b610a91ed5b
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2022-07-06 00:24:23 +0200

    Agregator logów & Dziedziczenie added

---
+++ University/C#/Agregator logów/Agregator logów/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agregator_logów
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, string> adres = new SortedDictionary<string, string>();
+            SortedDictionary<string, int> lista = new SortedDictionary<string, int>();
+            List<int> liczebnosc = new List<int>();
+            int _i = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tablica = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!adres.ContainsKey(tablica[0])) 
+                    adres.Add(tablica[0], tablica[1]);
+                if (!lista.ContainsKey(tablica[1])) 
+                    lista.Add(tablica[1], int.Parse(tablica[2]));
+                else 
+                    lista[tablica[1]] += int.Parse(tablica[2]);
+            }
+            foreach (var zmienna in lista)
+            {
+                int licznik = 0;
+
+                foreach (var _zmienna in adres)
+                {
+                    if (zmienna.Key == _zmienna.Value)
+                        licznik++;
+                }
+
+                liczebnosc.Add(licznik);
+            }
+            foreach (var zmienna in lista)
+            {
+                int j = 0;
+
+                Console.Write(zmienna.Key + ": " + zmienna.Value + " [");
+
+                foreach (var _zmienna in adres)
+                {
+                    
+                    if (zmienna.Key == _zmienna.Value)
+                    {
+                        if (j < liczebnosc[_i] - 1) Console.Write(_zmienna.Key + ", ");
+                        else Console.Write(_zmienna.Key);
+
+                        j++;
+                    }
+                }
+
+                Console.WriteLine("]");
+
+                _i++;
+            }
+        }
+    }
+}
---
+++ University/C#/Dziedziczenie/Dziedziczenie/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Dziedziczenie
+{
+    public class Person
+    {
+        public string FamilyName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string familyName, int age)
+        {
+            FirstName = firstName;
+            FamilyName = familyName;
+            Age = age;
+        }
+        public void modifyFirstName(string newFirstName)
+        {
+            FirstName = newFirstName;
+        }
+        public void modifyFamilyName(string newFamilyName)
+        {
+            FamilyName = newFamilyName;
+        }
+        public void modifyAge(int newAge)
+        {
+            Age = newAge;
+        }
+        public void print()
+        {
+            Console.WriteLine($"{FirstName} {FamilyName} ({Age})");
+        }
+    }
+    public class Child : Person
+    {
+        public Child(string firstName, string familyName, int age/*, Person mother, Person father*/) : base(firstName, familyName, age)
+        {
+        }
+        public string mother { get; set; }
+        public string father { get; set; }
+
+        public new void print()
+        {
+            Console.WriteLine($"{FirstName} {FamilyName} ({Age})");
+            Console.WriteLine($"mother: {FirstName} {FamilyName} ({Age})");
+            Console.WriteLine($"father: {FirstName} {FamilyName} ({Age})");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person o = new Person(familyName: "Molenda", firstName: "Krzysztof", age: 30);
+                Person m = new Person(familyName: "Molenda", firstName: "Ewa", age: 29);
+                Child d = new Child(familyName: "Molenda", firstName: "Anna", age: 10/*,
+                                    mother: m, father: o*/);
+                Console.WriteLine(d);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
